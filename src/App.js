0614102affdc4e5ac63c059c00c9acd026438eba
saidaloxon <acerqudrat1@gmail.com>
2023-03-13 22:39:02 +0500

    poop

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hooks from './components/Hooks'
+
+
+
+function App() {
+  return (
+    <div> 
+     
+      <Hooks/>
+     </div>
+
+  )
+}
+
+export default App
