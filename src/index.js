a5c86e48682058fc4e02d665c83fb1ecaadc85a7
saidaloxon <acerqudrat1@gmail.com>
2023-04-03 09:43:49 +0500

    togri

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StudentList from './components/Context';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StudentList>
+      <App />
+    </StudentList>
   </React.StrictMode>
 );
 
